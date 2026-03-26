d39162e953ec82ea22178f77b62ae2955c5678ec
nalraj2907 <nalraj20040.com>
2026-03-26 10:03:58 +0530

    commit

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ const markCommit = (x, y) => {
 };
 
 const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+  if(n===0) return simpleGit().push();    
+  const x = random.int(0, 54);    
+  const y = random.int(0, 6);                                                          
+  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();       
 
   const data = {
     date: date,
